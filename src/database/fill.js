3524fbb716db6869d4effe807533603c36ae447f
formation <mailys.pelissier@gmail.com>
2026-01-28 18:11:53 +0100

    insertion temps reel mongodb

--- src/database/fill.js
+++ src/database/fill.js
@@ -15,47 +15,66 @@ const FAKESONDE_FILE = '/dev/shm/sensors';
 let client;
 let collection;
 
-async function insert(){
 
-    // initialisation de la base de donnée
+async function init() {
+  /* 
+  Initialisation connection database
+  */
 
-    const client = new MongoClient(MONGO_URL)
-    try {
-    await client.connect();
+  client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log('connection à MongoDB réussie');
+  console.log('database :', DB_NAME, '; collection :', COLLECTION_NAME);
+  
+  const db = client.db(DB_NAME);
+  collection = db.collection(COLLECTION_NAME);
+}
 
-    const db = client.db(DB_NAME);
-    collection = db.collection(COLLECTION_NAME);
 
-    console.log('connection à MongoDB réussie');
-    console.log('database', DB_NAME, 'initialisée');
-    console.log('collection', COLLECTION_NAME);
 
-    // lecture du fichier
 
-    const fileContent = fs.readFileSync(FAKESONDE_FILE, 'utf8');
+
+async function insertData(filename) {
+    /* insertion des données dans la base de données */
+
+  try {
+    const fileContent = fs.readFileSync(filename, 'utf8');
     const weatherData = JSON.parse(fileContent);
-    console.log('lecture du fichier ?');
-
+    weatherData.insertedAt = new Date();
+    
     const result = await collection.insertOne(weatherData);
+    console.log(`[${new Date().toLocaleTimeString()}] Données insérées: ${result.insertedId}`);
+    
+  } catch (error) {
+    console.error('error', error.message);
+  }
+}
 
-    console.log(`Données insérées avec l'ID: ${result.insertedId}`);
-
-    console.log(JSON.stringify(weatherData, null, 2));
-
-    console.log(result)
-
-
+async function watchFile(filename) {
+  /* surveillance du fichier en entrée */
+  await init();
+  
+    console.log(`surveillance du fichier: ${filename}`);
+  
+  // Surveiller les changements du fichier
+  fs.watch(filename, async (eventType) => {
+    if (eventType === 'change') {
+      await insertData(filename);
     }
-    catch (error){
-        console.log('error', error.message);
-    }
+  });
+  
+  // Gestion de l'arrêt propre
+  process.on('SIGINT', async () => {
+    await client.close();
+    console.log('\n arrêt');
+    process.exit(0);
+  });
+}
+
+watchFile(FAKESONDE_FILE);
 
 
-
-
+function cleanDb(){
+    // TODO
 };
 
-
-
-
-insert();
