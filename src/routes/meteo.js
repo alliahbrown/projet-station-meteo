7a1b04971d49dc7f262cc4a5f967827adc136087
formation <mailys.pelissier@gmail.com>
2026-02-05 13:53:13 +0100

    modification version mongodb

--- src/routes/meteo.js
+++ src/routes/meteo.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 // GET /meteo/v1/live
 router.get('/v1/live', (req, res) => {
-  res.json({ message: 'Route live - à compléter' });
+  res.json({ message: 'Route live ' });
 });
 
 // GET /meteo/v1/archive
 router.get('/v1/archive', (req, res) => {
-  res.json({ message: 'Route archive - à compléter' });
+  res.json({ message: 'Route archive ' });
 });
 
 export default router;
