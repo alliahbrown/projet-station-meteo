dd97784279b14e7878f0a83d4ffce5883d945de5
formation <mailys.pelissier@gmail.com>
2026-02-05 11:02:35 +0100

    corrections mineures

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 import { connect } from './src/database/connexion.js';
 
 const app = express();
@@ -8,22 +6,6 @@ const PORT = process.env.API_PORT || 3000;
 
 app.use(express.json());
 
-
-// // Route de test
-// app.get('/', (req, res) => {
-//   res.json({ message: 'API Météo en ligne' });
-// });
-
-// // Route pour récupérer les données météo
-// app.get('/api/meteo', async (req, res) => {
-//   try {
-//     const db = await connect();
-//     const data = await db.collection('meteo').find().limit(100).toArray();
-//     res.json(data);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 // Démarrer le serveur
 async function startServer() {
